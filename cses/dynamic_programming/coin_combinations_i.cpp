1a31212012fd43a0122bc55d017f8b60cdbe8808
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-02-23 22:04:26 +0530

    Coin Combinations I solution

---
+++ cses/dynamic_programming/coin_combinations_i.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define endl '\n'
+#define forn(i, n) for(ll i = 0; i < n; i++)
+#define readi(e) int e; cin >> e
+#define readl(e) ll e; cin >> e
+#define reads(e) string e; cin >> e
+#define T int tt; cin >> tt; while(tt--)
+
+
+template<typename U>
+void print(U arr) {
+	for(auto element: arr) {
+		cout << element << " ";
+	}
+	cout << endl;
+}
+
+// read and write into files, rather than standard i/o
+void setup(string s) {
+	freopen((s+".in").c_str(), "r", stdin);
+	freopen((s+".out").c_str(), "w", stdout);
+}
+
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	const ll M = 1e9+7;
+	ll n, x;
+	cin >> n >> x;
+	vl v(n);
+	for(auto& e: v) cin >> e;
+
+	vl u(x+1, 0);
+	u[0] = 1;
+	for(int i = 1; i <= x; i++) {
+		for(auto& e: v) {
+			if(i-e >=0) {
+				u[i] += u[i-e];
+				u[i] = u[i]%M;
+			}
+		}
+	}
+	cout << u[x] << endl;
+
+	return 0;
+}
+
