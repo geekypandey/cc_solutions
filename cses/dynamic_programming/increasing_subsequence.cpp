50a375c597bbc66a5084a9ea8f32b101f152930f
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-03-16 14:39:19 +0530

    WA; TLE; Increasing Subsequence

---
+++ cses/dynamic_programming/increasing_subsequence.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define endl '\n'
+#define forn(i, n) for(ll i = 0; i < n; i++)
+#define fora(i, a, n) for(ll i = a; i < n; i++)
+#define readi(e) int e; cin >> e
+#define readl(e) ll e; cin >> e
+#define reads(e) string e; cin >> e
+#define T int tt; cin >> tt; while(tt--)
+
+
+template<typename U>
+void print(U arr) {
+	for(auto element: arr) {
+		cout << element << " ";
+	}
+	cout << endl;
+}
+
+// read and write into files, rather than standard i/o
+void setup(string s) {
+	freopen((s+".in").c_str(), "r", stdin);
+	freopen((s+".out").c_str(), "w", stdout);
+}
+
+const int M = 1e9+7;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vi v(n);
+	for(auto& e: v) cin >> e;
+	v.erase(unique(all(v)), v.end());
+	n = v.size();
+	vector<int> dp(n, 1);
+	forn(i, n) {
+		for(int j = 0; j < i; j++) {
+			if(v[i] > v[j]) {
+				dp[i] = max(dp[i], dp[j]+1);
+			}
+		}
+	}
+	cout << dp[n-1] << endl;
+
+	return 0;
+}
+
