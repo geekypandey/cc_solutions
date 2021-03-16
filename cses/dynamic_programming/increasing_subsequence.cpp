b5dcf98ea12a684d66423bd29747383c0801bf09
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-03-16 15:38:19 +0530

    AC; Increasing Subsequence using DP with Segment Tree

--- cses/dynamic_programming/increasing_subsequence.cpp
+++ cses/dynamic_programming/increasing_subsequence.cpp
@@ -45,24 +45,43 @@ const int mxN = 2e5;
 vi v, vals;
 int n;
 
-vi bit(mxN+1, 0);
+vi t(4*mxN+1);
+vi lo(4*mxN+1);
+vi hi(4*mxN+1);
 
-void upd(int i) {
-	int ind = upper_bound(all(vals), i) - vals.begin();
-	while(ind <= n) {
-		bit[ind] += 1;
-		ind += (ind&-ind);
+void build(int i=1, int l = 0, int r=n-1) {
+	lo[i] = l;
+	hi[i] = r;
+	if(l == r) {
+		t[i] = 0;
+		return;
 	}
+	int m = l+(r-l)/2;
+	build(2*i, l, m);
+	build(2*i+1, m+1, r);
 }
 
-int query(int i) {
-	int ind = upper_bound(all(vals), i) - vals.begin();
-	int s = 0;
-	while(ind > 0) {
-		s += bit[ind];
-		ind -= (ind&-ind);
+void upd(int i, int k, int val) {
+	if(lo[i] > k || hi[i] < k) {
+		return;
 	}
-	return s;
+	if(k<=lo[i] && hi[i]<=k) {
+		t[i] = max(t[i], val);
+		return;
+	}
+	upd(2*i, k, val);
+	upd(2*i+1, k, val);
+	t[i] = max(t[2*i], t[2*i+1]);
+}
+
+int query(int i, int l, int r) {
+	if(lo[i] > r || hi[i] < l) {
+		return INT_MIN;
+	}
+	if(l<=lo[i] && hi[i]<=r) {
+		return t[i];
+	}
+	return max(query(2*i, l, r), query(2*i+1, l, r));
 }
 
 int main(void) {
@@ -73,17 +92,19 @@ int main(void) {
 	v = vector<int>(n);
 	for(auto& e: v) cin >> e;
 	v.erase(unique(all(v)), v.end());
+	n = v.size();
 	vals = v;
 	sort(all(vals));
-	print(v);
+	vals.erase(unique(all(vals)), vals.end());
+	n = vals.size();
+	build();
 
-	n = v.size();
 	int ans = 0;
 	for(auto& e: v) {
-		upd(e);
-		int q = query(e);
-		cout << e << ' ' << q << endl;
-		ans = max(ans, q);
+		int ind = upper_bound(all(vals), e) - vals.begin();
+		int q = query(1, 0, ind-1);
+		upd(1, ind, q+1);
+		ans = max(ans, q+1);
 	}
 	cout << ans << endl;
 
